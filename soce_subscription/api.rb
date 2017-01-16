08fa48fb59893eb0c1ac2c120855be1becf275f3
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2017-01-16 10:09:41 +0100

    Set BasicAuth and api structure

--- soce_subscription/api.rb
+++ soce_subscription/api.rb
@@ -5,8 +5,21 @@ module SoceSubscription
     format :json
     prefix :api
 
-    get :hello do
-      { hello: 'world' }
+    http_basic do |user, password|
+      user == ENV['SOCE_API_USER'] && ENV['SOCE_API_PASSWORD']
+    end
+
+    resource :subscription_status do
+
+      desc 'Return subscription status of given user '
+      params do
+        requires :uuid, type: String , desc: 'user uuid.'
+      end
+      route_param :uuid do
+        get do
+          { hello: 'world' }
+        end
+      end
     end
 
   end
